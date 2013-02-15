5d70a543c45d0e424911b333d3fd9820e4d40410
James Thompson <james@plainprograms.com>
2013-02-15 11:52:36 -0700

    Added initial override and partial to add Cloud Files option to Image Settings in admin.

---
+++ app/overrides/add_cloud_files_options.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path:  'spree/admin/image_settings/edit',
+                     name:          'enable_rackspace_cloud_files',
+                     insert_after:  'div data-hook="use_s3"',
+                     partial:       'spree/admin/image_settings/add_cloud_files_options')
